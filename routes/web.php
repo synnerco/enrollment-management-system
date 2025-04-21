d05b3eef4be4d1560c2711e4e737cb29e26427bc
seanticzon <seanticzon@gmail.com>
2025-04-22 01:34:20 +0800

    Created the feature/studentController branch where it includes the new Controller for Students note: only the index work for now

--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,7 @@ Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::resource('students', \App\Http\Controllers\StudentController::class);
+
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
